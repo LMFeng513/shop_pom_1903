05475d15bd5f5431e9748d36864cec5050c78e09
LMFeng <1574881211@qq.com>
2019-07-06 14:45:51 +0800

    商品管理无服务

--- shop_web/shop_back/src/main/java/com/qf/controller/GoodsController.java
+++ shop_web/shop_back/src/main/java/com/qf/controller/GoodsController.java
@@ -6,16 +6,26 @@ package com.qf.controller;
     @desc:
   */
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.qf.entity.Goods;
+import com.qf.service.IGoodsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/goods")
 public class GoodsController {
 
+        @Reference
+        private IGoodsService goodsService;
+
         @RequestMapping("/list")
-        public String list()
-        {
-          return "list";
+        public String goodslist(Model model) {
+            List<Goods> goodsList = goodsService.queryGoodsList();
+           model.addAttribute("goods",goodsList);
+            return "goodslist";
         }
 }
